55d776d959c204e3e533f0feedf6c48f711af1d9
gerproko <101721169+GerProko@users.noreply.github.com>
2022-05-20 10:35:39 -0300

    casiTerminado

---
+++ Assets/New Folder/PlataformaMovimiento.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlataformaMovimiento : MonoBehaviour
+{
+    public float zSpeed;
+    public bool toRight;
+
+    public GameObject prefabSphere;
+    public GameObject prefabCylinder;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (toRight == true)
+        {
+        transform.position -= new Vector3(0, 0, zSpeed);
+        }
+        else
+        {
+        transform.position += new Vector3(0, 0, zSpeed);
+        }
+        if (transform.position.z > 3)
+        {
+            toRight = true;
+            Instantiate(prefabSphere);
+        }
+        if (transform.position.z < -3)
+        {
+            toRight = false;
+            Instantiate(prefabCylinder);
+        }
+    }
+}
